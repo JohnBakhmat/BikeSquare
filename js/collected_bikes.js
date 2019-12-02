8499059dffd0a2696b2c5348f02965925e47792d
MrDiviner <yevhenii.bakhmat@nure.ua>
2019-12-02 12:09:08 +0200

    Из корзины теперь можно убрать товар

--- js/collected_bikes.js
+++ js/collected_bikes.js
@@ -1,6 +1,19 @@
-var save = [];
-  function collect(id) {
-    save.push(id);
-    return alert("Вы выбрали:"+"\n"+save);
-  }
+var save= [];
+function js (inp) {
 
+  if(save[inp-1]===undefined)
+    return collect(inp-1);
+  else
+    return del(inp-1);
+
+  //Collector
+  function collect ( id ) {
+    save[id]=id+1;
+    return alert ("Вы выбрали:" + "\n" + save.join(' '));
+  }
+  //Anihilator
+  function del ( id ) {
+    delete save[id];
+    return alert ("Вы выбрали:" + "\n" + save.join(' '));
+  }
+}
